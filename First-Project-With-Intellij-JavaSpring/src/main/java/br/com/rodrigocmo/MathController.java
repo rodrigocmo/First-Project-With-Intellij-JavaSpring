3ba728e397ee9d18863f194a152b92e354216932
rodrigocmo <rcastromorais01@gmail.com>
2022-11-15 14:13:25 -0300

    commit com gitignore arrumado

--- First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/MathController.java
+++ First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/MathController.java
@@ -1,18 +1,19 @@
 package br.com.rodrigocmo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-@RestController
-public class GreetingController {
+@RestController(value = "/math")
+public class MathController {
     private static final String template = "Hello, %s!";
     private static AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value = "name", defaultValue = "Word") String name){
+    @GetMapping("/sum/{numberOne}/{numeberTwo}")
+    public Double sum(@RequestParam(value = "name", defaultValue = "Word") String name){
         return new Greeting(counter.incrementAndGet(), String.format(template,name));
     }
 }

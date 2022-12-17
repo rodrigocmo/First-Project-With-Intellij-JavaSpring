14ce7807c7b5e5a448f5788a6a921550cb1ecd8c
Rodrigo Morais <rcastromorais01@gmail.com>
2022-12-16 21:25:49 -0300

    .gitignore is now working

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,7 +1,3 @@
 
-]
--src/main/java/br/com/rodrigocmo/Greeting.java,d\3\d3f0f081af0739d6af8c6f7371db6fb097d04315
 g
 7src/main/java/br/com/rodrigocmo/GreetingController.java,8\6\8646cf4340956345f9086ccd00b3c44a8065672f
-\
-,src/main/java/br/com/rodrigocmo/Startup.java,3\5\3580d243ac737e159069688fa161a5cd78867945
--- First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/MathController.java
+++ First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/MathController.java
@@ -1,19 +1,16 @@
 package br.com.rodrigocmo;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.atomic.AtomicLong;
-
-@RestController(value = "/math")
+@RestController
 public class MathController {
-    private static final String template = "Hello, %s!";
-    private static AtomicLong counter = new AtomicLong();
 
-    @GetMapping("/sum/{numberOne}/{numeberTwo}")
-    public Double sum(@RequestParam(value = "name", defaultValue = "Word") String name){
-        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    @RequestMapping(value="/sum/{numberOne}/{numberTwo}", method= RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne,
+                      @PathVariable("numberTwo") String numberTwo) {
+        return 1d;
     }
 }

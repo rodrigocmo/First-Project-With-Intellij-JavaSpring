609289ec821fe229c7eda343125eed7ba301ff78
Rodrigo Morais <rcastromorais01@gmail.com>
2023-01-12 18:30:08 -0300

    Inicio dos trabalhos com banco

---
+++ First-Project-With-Intellij-JavaSpring/src/main/java/db/Person.java
@@ -0,0 +1,32 @@
+package db;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+
+@Entity
+@Table(name = "tb_person")
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "first_name", nullable = false, length = 80)
+    private String firstname;
+    @Column(name = "last_name", length = 80)
+    private String lastName;
+    @Column
+    private String address;
+    @Column
+    private String gender;
+
+}

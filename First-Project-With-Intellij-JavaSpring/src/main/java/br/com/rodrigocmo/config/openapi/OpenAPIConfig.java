3ab2d4a2e37d85a2d9d1db220c0c304557f08d13
Rodrigo Morais <rcastromorais01@gmail.com>
2023-03-08 23:02:27 -0300

    commit testcontainer com swagger

--- First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/config/openapi/OpenAPIConfig.java
+++ First-Project-With-Intellij-JavaSpring/src/main/java/br/com/rodrigocmo/config/openapi/OpenAPIConfig.java
@@ -1,14 +1,12 @@
-package br.com.rodrigocmo.config;
+package br.com.rodrigocmo.config.openapi;
 
-import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Configuration()
-@RequestMapping(path = "/api-docs")
 public class OpenAPIConfig {
 
     @Bean
